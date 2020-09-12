04da4572fe3a7cc50f6d6ce89af139c6e2c5e296
jakefernihough <jakefernihough@gmail.com>
2020-09-12 12:31:30 +0000

    created a form using Emailjs and linking it to an email template. when you submit a message, an email will go through to the website email address as well as return an automated email to the sender

--- assets/js/sendEmail.js
+++ assets/js/sendEmail.js
@@ -1,15 +1,15 @@
 function sendMail(contactForm) {
-    emailjs.send("gmail", "black_country_canals", {
-       "from_name": contactForm.name.value,
-       "from_email": contactForm.emailaddress.value,
-       "project_request": contactForm.projectsummary.value
+    emailjs.send("gmail","template_1b5075x", {
+        "from_name": contactForm.name.value,
+        "from_email": contactForm.emailaddress.value,
+        "project_request": contactForm.informationsummary.value
     })
     .then(
         function(response) {
             console.log("SUCCESS", response);
         },
         function(error) {
-            console.log("FAILED", error)
+            console.log("FAILED", error);
         });
-        return false;
+    return false;
 }
