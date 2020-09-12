ac776a1aff2a6b63bedd882db6aa6161dabcab46
jakefernihough <jakefernihough@gmail.com>
2020-09-12 13:11:31 +0000

    fixed problem where the contact form did not refresh after sending email

--- assets/js/sendEmail.js
+++ assets/js/sendEmail.js
@@ -1,3 +1,7 @@
+let nameInput = document.getElementById("fullname");
+let emailInput = document.getElementById("emailaddress");
+let informationsummaryInput = document.getElementById("informationsummary");
+
 function sendMail(contactForm) {
     emailjs.send("gmail","template_1b5075x", {
         "from_name": contactForm.name.value,
@@ -5,7 +9,11 @@ function sendMail(contactForm) {
         "project_request": contactForm.informationsummary.value
     })
     .then(
+        
         function(response) {
+            nameInput.value = '',
+            emailInput.value='',
+            informationsummaryInput.value='',
             console.log("SUCCESS", response);
         },
         function(error) {
